01816fccda08d2a0251257aee520a5b846f906d7
Karl Stolley <karl.stolley@gmail.com>
2018-08-23 16:38:07 -0500

    Correct spacing on full-calendar header; bump SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '0.0.1';
+const version = '0.0.3';
 // Detect the service worker's location to set a scope for absolute paths
 const detected_scope = '/'+location.href.split('/')[3]+'/';
 
